0ea0c4edae91879e81d26a51f6b77561e06ece72
Alec Musasa <alecmus@live.com>
2023-05-10 12:20:37 +0200

    Demonstrating the Spring Context

--- src/main/java/com/github/alecmus/spring6di/Spring6DiApplication.java
+++ src/main/java/com/github/alecmus/spring6di/Spring6DiApplication.java
@@ -1,13 +1,19 @@
 package com.github.alecmus.spring6di;
 
+import com.github.alecmus.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6DiApplication.class, args);
+        ApplicationContext ctx =  SpringApplication.run(Spring6DiApplication.class, args);
+
+        MyController controller = ctx.getBean(MyController.class);
+        System.out.println("In Main Method");
+        System.out.println(controller.sayHello());
     }
 
 }
---
+++ src/main/java/com/github/alecmus/spring6di/controllers/MyController.java
@@ -0,0 +1,11 @@
+package com.github.alecmus.spring6di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello() {
+        System.out.println("I'm in the controller");
+        return "Hello Everyone!!!";
+    }
+}
