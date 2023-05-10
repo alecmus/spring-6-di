d2d1cb8e6a57f1c47dab7a9e40e3fe237f8edd91
Alec Musasa <alecmus@live.com>
2023-05-10 12:31:16 +0200

    Demonstrating the Spring Test Context

--- src/test/java/com/github/alecmus/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/com/github/alecmus/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,29 @@
 package com.github.alecmus.spring6di;
 
+import com.github.alecmus.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutoWiredOfController() {
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromCtx() {
+        MyController myController = applicationContext.getBean(MyController.class);
+        System.out.println(myController.sayHello());
+    }
 
     @Test
     void contextLoads() {
