29ced146b8f93fd7fe807cce231e24792310fccb
Alec Musasa <alecmus@live.com>
2023-05-12 15:10:03 +0200

    Demonstrating Spring profiles

---
+++ src/main/java/com/github/alecmus/spring6di/services/i18n/EnglishGreetingService.java
@@ -0,0 +1,14 @@
+package com.github.alecmus.spring6di.services.i18n;
+
+import com.github.alecmus.spring6di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
---
+++ src/test/java/com/github/alecmus/spring6di/controllers/i18n/myi18NControllerTestES.java
@@ -0,0 +1,20 @@
+package com.github.alecmus.spring6di.controllers.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("ES")
+@SpringBootTest
+class myi18NControllerTestES {
+    @Autowired
+    Myi18NController myi18NController;
+
+    @Test
+    void sayHello() {
+        System.out.println(myi18NController.sayHello());
+    }
+}
